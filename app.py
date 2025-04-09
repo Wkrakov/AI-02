3363eb6fb2d11bb47e05ca01a11530f32fbbcf4d
Wkrakov <babzooles@mail.ru>
2025-04-09 21:28:11 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import easyocr
+from PIL import Image
+import io
+
+def load_image():
+    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+st.title('Распознай английский текст с изображения!')
+img = load_image()
+
+result = st.button('Распознать изображение')
+if result:
+    if img is not None:
+        reader = easyocr.Reader(['en'])  # Создаем объект для распознавания текста на английском
+        result = reader.readtext(np.array(img))  # Преобразуем PIL Image в numpy array
+        text = ' '.join([item[1] for item in result])  # Извлекаем текст из результатов
+        st.write('Результаты распознавания:')
+        st.write(text)
+    else:
+        st.write("Пожалуйста, загрузите изображение.")
